fa0a19452b235d7a0a7f1aa1da211d56aa8227b7
flyingyin <yin.cas@gmail.com>
2024-12-26 11:29:10 -0800

    change download default

--- cmd/dl.go
+++ cmd/dl.go
@@ -52,14 +52,14 @@ func NewDownload() *cobra.Command {
 	cmd.Flags().StringSliceVarP(&opts.Include, include, "i", []string{}, "include the specified file extensions, and only judge by file name, not file MIME. Example: -i mp4,mp3")
 	cmd.Flags().StringSliceVarP(&opts.Exclude, exclude, "e", []string{}, "exclude the specified file extensions, and only judge by file name, not file MIME. Example: -e png,jpg")
 
-	cmd.Flags().StringVarP(&opts.Dir, dir, "d", "downloads", "specify the download directory. If the directory does not exist, it will be created automatically")
+	cmd.Flags().StringVarP(&opts.Dir, dir, "d", ".", "specify the download directory. If the directory does not exist, it will be created automatically")
 	cmd.Flags().BoolVar(&opts.RewriteExt, "rewrite-ext", false, "rewrite file extension according to file header MIME")
 	// do not match extension, because some files' extension is corrected by --rewrite-ext flag
-	cmd.Flags().BoolVar(&opts.SkipSame, "skip-same", false, "skip files with the same name(without extension) and size")
+	cmd.Flags().BoolVar(&opts.SkipSame, "skip-same", true, "skip files with the same name(without extension) and size")
 
 	cmd.Flags().BoolVar(&opts.Desc, "desc", false, "download files from the newest to the oldest ones (may affect resume download)")
 	cmd.Flags().BoolVar(&opts.Takeout, "takeout", false, "takeout sessions let you export data from your account with lower flood wait limits.")
-	cmd.Flags().BoolVar(&opts.Group, "group", false, "auto detect grouped message and download all of them")
+	cmd.Flags().BoolVar(&opts.Group, "group", true, "auto detect grouped message and download all of them")
 
 	// resume flags, if both false then ask user
 	cmd.Flags().BoolVar(&opts.Continue, _continue, false, "continue the last download directly")
